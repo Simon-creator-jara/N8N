da6dcad2b7f3e936d6d67c82b89adfc1b1a3dc99
Simon jaramillo Velez <simjaram@bancolombia.com.co>
2025-10-05 12:27:47 -0500

    Adding new setting

--- run_local.sh
+++ run_local.sh
@@ -1,2 +1,5 @@
 #! /usr/bin/env bash
-docker-compose -f deployment/compose.yaml --project-directory . up --build
+mkdir -p deployment/n8n_data/custom/node_modules
+rm -rf deployment/n8n_data/custom/node_modules/n8n_nodes_Lib
+cp -R /Users/simonjaramillovelez/NU1009002_N8N_MR/n8n_nodes_Lib/. deployment/n8n_data/custom/node_modules/
+docker compose -f deployment/compose.yaml --project-directory . up --build
